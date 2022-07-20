10705267d3c01e051268148ff25e5ea75edbe0c2
stinkyfi <stinkyzeroex@gmail.com>
2022-07-19 20:45:22 -0700

    Updating docs

--- contracts/TVAF.sol
+++ contracts/TVAF.sol
@@ -9,31 +9,49 @@ import "./ERC721A/ERC721A.sol";
 contract TVAF is ERC721A, Ownable, Pausable {
     using Counters for Counters.Counter;
 
-    // @notice Counter for number of minted characters
-    Counters.Counter public _tokenIds;    
-    // Max Supply of DegenDwarfs
+    /// @notice Counter for number of minted Frens
+    Counters.Counter public tokenIds;    
+    /// @notice Max Supply of Frens (immutable)
     uint256 public immutable maxSupply = 7799;
+    /// @notice NFT Mint Price
     uint256 public mintPrice;
+    /// @notice Artist Wallet
     address private artWallet;
+    /// @notice Dev Wallet
     address private devWallet;
-    string private _baseTokenUri;     
+    /// @notice NFT's Base Token URI
+    string private baseTokenUri;  
 
+    error EmptyBalance();
+    error IncorrectAmount();
+    error InvalidQuantity();
+    error MintedOut();
+
+    /// @notice Deploy ERC-721A contract and initialize some values
+    /// @param _tokenURI The initial global TokenURI
+    /// @param _dev The developer address
+    /// @param _art The artist address
     constructor(string memory _tokenURI, address _dev, address _art) ERC721A("The Vinnie And Frens", "TVAF") {
-        _baseTokenUri = _tokenURI;
+        baseTokenUri = _tokenURI;
         //Mint 100 at deployment? to Deployer wallet?
         _mint(_msgSender(), 100);
         mintPrice = 0.01 ether;
         devWallet = _dev;
         artWallet = _art;
-
     }
 
-    function mint(uint256 _quantity) external payable {
-        require(msg.value == mintPrice, "Invalid Balance");
+    /// @notice Main Mint function
+    /// @param _quantity How many mints would you like?
+    function mint(uint256 _quantity) external payable whenNotPaused {
+        if(_quantity <= 0) { revert InvalidQuantity(); }
+        if(tokenIds.current() + _quantity >= maxSupply) { revert MintedOut(); }
+        if(msg.value < mintPrice) { revert IncorrectAmount(); }
         _mint(_msgSender(), _quantity);
     }
 
+    /// @notice Withdraw funds payment split between Art and Devs
     function withdraw() external onlyOwner {
+        if(address(this).balance <= 0) { revert EmptyBalance(); }
         uint256 artPay = (address(this).balance * (1e18 - 0.6 ether)) / 1e18;
         uint256 devPay = (address(this).balance * (1e18 - 0.4 ether)) / 1e18;
         //Add logic
@@ -41,20 +59,16 @@ contract TVAF is ERC721A, Ownable, Pausable {
         payable(devWallet).transfer(devPay);
     }
 
+    /// @notice Update the Mint price
+    /// @param _newPrice The new mint price
     function updatePrice(uint256 _newPrice) external onlyOwner {
         mintPrice = _newPrice;
     }
  
-    function setBaseURI(string memory baseURI) external onlyOwner {
-        _baseTokenUri = baseURI;
-    }  
-
-    function pauseMinting() external onlyOwner {
-        _pause();
-    }
-
-    function unpauseMinting() external onlyOwner {
-        _unpause();
+    /// @notice Update the base URI
+    /// @param _baseURI The new base URI
+    function setBaseURI(string memory _baseURI) external onlyOwner {
+        baseTokenUri = _baseURI;
     }  
    
     function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
