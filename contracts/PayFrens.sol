cd639076f8fbcfaefaaebdc3504c4d00a166d5c2
Stinky <83800142+stinkyfi@users.noreply.github.com>
2022-08-03 20:10:38 -0700

    Update PayFrens.sol

--- contracts/PayFrens.sol
+++ contracts/PayFrens.sol
@@ -1,5 +1,4 @@
-// SPDX-License-Identifier: MIT
-// OpenZeppelin Contracts (last updated v4.7.0) (finance/PaymentSplitter.sol)
+// SPDX-License-Identifier: Unlicense
 /*   _     _      _     _      _     _      _     _      _     _     &     _     _      _     _      _     _      _     _      _     _   
   (c).-.(c)    (c).-.(c)    (c).-.(c)    (c).-.(c)    (c).-.(c)         (c).-.(c)    (c).-.(c)    (c).-.(c)    (c).-.(c)    (c).-.(c)  
    / ._. \      / ._. \      / ._. \      / ._. \      / ._. \           / ._. \      / ._. \      / ._. \      / ._. \      / ._. \   
@@ -13,13 +12,4 @@
 
 pragma solidity ^0.8.0;
 
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/finance/PaymentSplitter.sol";
-
-contract PayFrens is PaymentSplitter {
-
-constructor (address[] memory _payees, uint256[] memory _shares) PaymentSplitter(_payees, _shares) payable {}
-
-}
-
-//  ["0x4D6227fCba4c25FEac2B6EA347Ebd7851B781EDf","0xEd4aAA18435912de59d84cDA5996279a7e9Cd54F"]
-// [60,40]
+import "@openzeppelin/contracts/finance/PaymentSplitter.sol";
